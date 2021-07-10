f0db6f123b59e2a5baa377a69493196b07dd621b
Ram Janakarajan <rajanaka@microsoft.com>
2021-07-10 23:44:08 +0530

    Commit to the dev branch

--- demoapp/scripts/gethelmchart.ps1
+++ demoapp/scripts/gethelmchart.ps1
@@ -1,4 +1,4 @@
-param ($KVName, $ClientId, $ChartVersion)
+param ($KVName, $ClientId, $ChartVersion, $TenantId)
 $ErrorActionPreference = "SilentlyContinue"
 $env:HELM_EXPERIMENTAL_OCI=1
             
@@ -6,7 +6,7 @@ $response = Invoke-WebRequest -Uri "http://169.254.169.254/metadata/identity/oau
 $content = $response.Content | ConvertFrom-Json
 $ArmToken = $content.access_token
 
-Add-AzAccount -TenantId '72f988bf-86f1-41af-91ab-2d7cd011db47' -KeyVaultAccessToken $ArmToken -AccessToken $ArmToken -AccountId '4912e077-c99f-412f-89fb-640de8bf5198' |  out-null
+Add-AzAccount -TenantId $TenantId -KeyVaultAccessToken $ArmToken -AccessToken $ArmToken -AccountId $ClientId |  out-null
 
 $SP_Name = Get-AzKeyVaultSecret -VaultName $KVName -Name sp-name -AsPlainText
 $Sp_Secret = Get-AzKeyVaultSecret -VaultName $KVName -Name sp-secret -AsPlainText
